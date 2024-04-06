40c8f5c0f6362045f683283eb8d045c6dd8ef1f0
Lalith <lalithae30@gmail.com>
2024-04-06 18:26:05 +0530

    Improve things

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from sqlalchemy.sql.ddl import exc
 from uxsim import *
 import csv
 import logging
 
 # logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__);
+logger = logging.getLogger(__name__)
 
-SIMULATION_DURATION = 3600;
+SIMULATION_DURATION = 3600
+
 
 def gen_links_from_csv(W, fname):
     """
@@ -18,35 +18,57 @@ def gen_links_from_csv(W, fname):
         The file name of the CSV file containing link data.
     """
     with open(fname) as f:
-        succ = 0;
-        total = 0;
+        succ = 0
+        total = 0
         for r in csv.reader(f):
             if r[3] != "length":
                 try:
-                    W.addLink(r[0], r[1], r[2], length=float(r[3]), free_flow_speed=float(r[4]), jam_density=float(r[5]), merge_priority=float(r[6]));
-                    succ += 1;
+                    W.addLink(
+                        r[0],
+                        r[1],
+                        r[2],
+                        length=float(r[3]),
+                        free_flow_speed=float(r[4]),
+                        jam_density=float(r[5]),
+                        merge_priority=float(r[6]),
+                    )
+                    succ += 1
                 except Exception as e:
-                    logger.warn(f"Failed to add link {r},\n\tcause: {e}");
-            total += 1;
-        logger.info(f"Imported {succ} links, success rate {succ / total} [{total - succ} fails]");
+                    logger.warn(f"Failed to add link {r},\n\tcause: {e}")
+            total += 1
+        logger.info(
+            f"Imported {succ} links, success rate {succ / total} [{total - succ} fails]"
+        )
 
 
 # Define the main simulation
 # Units are standardized to seconds (s) and meters (m)
 W = World(
-    name="",    # Scenario name
-    deltan=5,   # Simulation aggregation unit delta n
+    name="",  # Scenario name
+    deltan=5,  # Simulation aggregation unit delta n
     tmax=SIMULATION_DURATION,  # Total simulation time (s)
-    print_mode=1, save_mode=1, show_mode=0,    # Various options
-    random_seed=0xb00b  # Set the random seed
+    print_mode=1,
+    save_mode=1,
+    show_mode=0,  # Various options
+    random_seed=0xBA115,  # Set the random seed
 )
 
-W.generate_Nodes_from_csv("osm/map_nodes.csv");
-gen_links_from_csv(W, "osm/map_edges.csv");
+W.generate_Nodes_from_csv("osm/map_nodes.csv")
+gen_links_from_csv(W, "osm/map_edges.csv")
 
 W.show_network(network_font_size=1)
 
-W.adddemand_area2area(13.001867215947623, 77.56709046386388, 0, 12.999073447375757, 77.5724257603931, 0.05, 0, 3600, volume=5000)
+W.adddemand_area2area(
+    13.001867215947623,
+    77.56709046386388,
+    0,
+    12.999073447375757,
+    77.5724257603931,
+    0.05,
+    0,
+    3600,
+    volume=5000,
+)
 
-W.exec_simulation();
-W.analyzer.output_data("out/sim");
+W.exec_simulation()
+W.analyzer.output_data("out/sim")
--- osm2csv.py
+++ osm2csv.py
@@ -3,20 +3,21 @@ import pandas as pd
 import logging
 import math
 
-logger = logging.getLogger(__name__);
+logger = logging.getLogger(__name__)
 
-RAD_DIST = 1.5; # Get Radius of region around required place.
+RAD_DIST = 1.5
+# Get Radius of region around required place.
 
 FREE_FLOW_SPEED_MAP = {
-    "highway":      28,
-    "residential":  7,
-    "primary":      12,
-    "secondary":    9,
-    "tertiary":     4
-};
+    "highway": 28,
+    "residential": 7,
+    "primary": 12,
+    "secondary": 9,
+    "tertiary": 4,
+}
 
-DEFAULT_FREE_FLOW_SPEED = 5;
-GLOBAL_BASE_JAM_DENSITY = 0.16;
+DEFAULT_FREE_FLOW_SPEED = 5
+GLOBAL_BASE_JAM_DENSITY = 0.16
 
 """
 IMPL Ideas:
@@ -24,6 +25,7 @@ IMPL Ideas:
     2. Add variance to free flow speed.
     3. Make demand non-uniform.
 """
+
 
 def local_coords(p1, p0, R=6_366_707):
     """
@@ -34,34 +36,34 @@ def local_coords(p1, p0, R=6_366_707):
     # lat2, lon2 = p0;
     # # Convert latitude and longitude from degrees to radians
     # lat1, lon1, lat2, lon2 = map(lambda x: x/180*math.pi, [lat1, lon1, lat2, lon2])
-    
+
     # # Haversine formula
     # dlat = lat2 - lat1
     # dlon = lon2 - lon1
     # a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
     # c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
     # distance = 6371 * c  # Earth radius in kilometers
-    
-    theta, phi0 = p0;
-    theta1, phi1= p1;
 
-    theta, phi0, theta1, phi1 = (x*math.pi/180 for x in (theta, phi0, theta1, phi1));
+    theta, phi0 = p0
+    theta1, phi1 = p1
 
-    Tdiff = theta1 - theta;
-    Pdiff = phi1 - phi0;
-    dx = R*math.cos(theta)*Pdiff;
-    dy = R*Tdiff;
-    print(f"p1: {p1}, p0: {p0} are {(dx, dy)}");
-    return (Tdiff, Pdiff);
+    theta, phi0, theta1, phi1 = (x * math.pi / 180 for x in (theta, phi0, theta1, phi1))
+
+    Tdiff = theta1 - theta
+    Pdiff = phi1 - phi0
+    dx = R * Pdiff
+    dy = R * Tdiff
+    print(f"p1: {p1}, p0: {p0} are {(dx, dy)}")
+    return (dx, dy)
 
 
 def get_lat_lon(location_name):
     """
     Get latitude and longitude details of a location using OpenStreetMap via OSMnx.
-    
+
     Parameters:
     location_name: Name or address of the location.
-    
+
     Returns:
     Tuple containing latitude and longitude details (latitude, longitude).
     """
@@ -69,14 +71,16 @@ def get_lat_lon(location_name):
         # Retrieve the point of interest from OpenStreetMap
         return ox.geocode(location_name)
     except Exception as e:
-        logger.error(f"Failed to geocode {location_name}");
-        return (None, None);
+        logger.error(f"Failed to geocode {location_name}")
+        return (None, None)
+
 
 def node_proc(nodes_iter, center):
     for node in nodes_iter:
-        print("Node", node);
-        p1 = node[1]['x'], node[1]['y'];
-        yield (node[0], *local_coords(p1, center));
+        print("Node", node)
+        p1 = node[1]["x"], node[1]["y"]
+        yield (node[0], *local_coords(p1, center))
+
 
 def edge_proc(edges_iter):
     """
@@ -85,49 +89,70 @@ def edge_proc(edges_iter):
     """
 
     for i, edge in enumerate(edges_iter):
-        name = f"E{i}";
-        road_len = edge[2]['length'];
-        rtype = str(edge[2]['highway']);
-        free_flow = FREE_FLOW_SPEED_MAP.get(rtype, DEFAULT_FREE_FLOW_SPEED);
-        print(name, ":", edge);
-        yield (name, edge[0], edge[1], float(road_len), float(free_flow), GLOBAL_BASE_JAM_DENSITY, 1);
+        name = f"E{i}"
+        road_len = edge[2]["length"]
+        rtype = str(edge[2]["highway"])
+        free_flow = FREE_FLOW_SPEED_MAP.get(rtype, DEFAULT_FREE_FLOW_SPEED)
+        print(name, ":", edge)
+        yield (
+            name,
+            edge[0],
+            edge[1],
+            float(road_len),
+            float(free_flow),
+            GLOBAL_BASE_JAM_DENSITY,
+            1,
+        )
+
 
 def export_to_csv(graph, center, filename):
     """
     Export graphs into two csv files
     """
-    
-    a = node_proc(graph.nodes(data=True), center);
-    b = edge_proc(graph.edges(data=True));
 
-    nodes_df = pd.DataFrame(a, columns=['node_id', 'x', 'y']);
-    edges_df = pd.DataFrame(b, columns=['name', 'source', 'target', 'length', 'free_flow_speed', 'jam_density', 'merge_priority']);
+    a = node_proc(graph.nodes(data=True), center)
+    b = edge_proc(graph.edges(data=True))
 
-    logger.info("Generated DataFrames.");
-    
+    nodes_df = pd.DataFrame(a, columns=["node_id", "x", "y"])
+    edges_df = pd.DataFrame(
+        b,
+        columns=[
+            "name",
+            "source",
+            "target",
+            "length",
+            "free_flow_speed",
+            "jam_density",
+            "merge_priority",
+        ],
+    )
+
+    logger.info("Generated DataFrames.")
+
     # Export DataFrames to CSV
-    nodes_df.to_csv(f'{filename}_nodes.csv', index=False)
-    edges_df.to_csv(f'{filename}_edges.csv', index=False)
+    nodes_df.to_csv(f"{filename}_nodes.csv", index=False)
+    edges_df.to_csv(f"{filename}_edges.csv", index=False)
 
-    return (nodes_df, edges_df);
+    return (nodes_df, edges_df)
 
 
 def find_graph(addr: str):
-    logger.info("Querying graph from OSM.");
+    logger.info("Querying graph from OSM.")
     s = ox.graph_from_address(
-        addr, 
-        dist=int(RAD_DIST*1000),
+        addr,
+        dist=int(RAD_DIST * 1000),
         dist_type="network",
         network_type="drive",
         simplify=True,
         retain_all=False,
         truncate_by_edge=False,
-    );
-    logger.info("Obtained result.");
-    loc = get_lat_lon(addr)[::-1];
-    return s, loc;
+    )
+    logger.info("Obtained result.")
+    loc = get_lat_lon(addr)[::-1]
+    return s, loc
+
 
 if __name__ == "__main__":
-    g, loc = find_graph("Malleshwaram, Bengaluru, India");
-    logger.info("Collecting and Exporting to CSV..");
-    ndf, edf = export_to_csv(g, loc, "osm/map");
+    g, loc = find_graph("Malleshwaram, Bengaluru, India")
+    logger.info("Collecting and Exporting to CSV..")
+    ndf, edf = export_to_csv(g, loc, "osm/map")
